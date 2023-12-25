ef5727e4098d932b3fb6d3bb0aaa9979f2a892f2
kimhyeok <kimmang940715@naver.com>
2023-12-26 06:37:45 +0900

    함수구현

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+// [반환타입] [함수이름] ([입력...]) { [구현부] } 
+
+int Test(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
