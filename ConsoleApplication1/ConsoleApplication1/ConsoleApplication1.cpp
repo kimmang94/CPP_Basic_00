47d1e49648fa9a5e1dbac8fd2af8925f54fc6e9e
kimhyeok <kimmang940715@naver.com>
2023-12-28 03:56:16 +0900

    배열 실습을 위한 초기화

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,19 +6,5 @@ using namespace std;
 
 int main()
 {
-	int arr[] = { 1, 2, 3, 4, 5 };
 
-	for (int i = 0; i < 5; i++)
-	{
-		cout << "arr 출력" << endl;
-		cout << arr[i] << endl;
-	}
-
-	int arr2[10] = { 1, 2, 3, 4, 5, 6 };
-
-	for (int i = 0; i < 5; i++)
-	{
-		cout << "arr2 출력" << endl;
-		cout << arr[i] << endl;
-	}
 }
